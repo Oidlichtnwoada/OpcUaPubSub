6e1f625bcf3cce553bf94f1e9be388376d552906
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2020-09-19 10:47:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build -t opc_ua_pub_sub:latest ./docker && \
+docker build -t opc_ua_pub_sub:latest ./src && \
 ID=$(docker run -id opc_ua_pub_sub:latest) && \
 docker cp "$ID":/open62541/build/opcua_publisher_arm ./build && \
 docker cp "$ID":/open62541/build/opcua_subscriber_arm ./build && \
-docker cp "$ID":/open62541/build/open62541.h ./docker/open62541.h && \
-docker cp "$ID":/open62541/build/open62541.c ./docker/open62541.c && \
+docker cp "$ID":/open62541/build/open62541.h ./src/open62541.h && \
+docker cp "$ID":/open62541/build/open62541.c ./src/open62541.c && \
 docker kill "$ID"
